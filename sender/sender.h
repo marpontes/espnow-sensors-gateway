f5e15aca695722fde10dd752d114d4ff7d746667
Marcello Pontes <balaum@gmail.com>
2020-05-15 13:02:33 +0200

    Increased sleep time for the edge sensors

--- sender/sender.h
+++ sender/sender.h
@@ -4,7 +4,7 @@
 
 //Deepsleep
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  4        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  20        /* Time ESP32 will go to sleep (in seconds) */
 
 RTC_DATA_ATTR bool debug = true;
 RTC_DATA_ATTR uint8_t receiverAddr[] = {0x84, 0x0D, 0x8E, 0xE6, 0x89, 0x09};
