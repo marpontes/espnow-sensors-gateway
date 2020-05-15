f0bae400451e8a0920b94329d4887bafad2e5ad0
Marcello Pontes <balaum@gmail.com>
2020-05-15 13:03:13 +0200

    Added a little helper for testing c features quickly

---
+++ receiver/helpers.c
@@ -0,0 +1,21 @@
+// #import <stdio.h>
+// #import <string.h>
+// #import <stdlib.h>
+// #define MACSIZE 6
+// #define MACSTRSIZE 18
+
+// int registeredPeers[][MACSIZE] = {
+//   {0x4C, 0x11, 0xAE, 0xEA, 0x6E, 0x48},
+//   {0x4C, 0x11, 0xAE, 0xEA, 0x6E, 0x43},
+//   {0x4C, 0x11, 0xAE, 0xEA, 0x6E, 0x42},
+//   {0x4C, 0x11, 0xAE, 0xEA, 0x6E, 0x41}
+// };
+
+
+
+// int main(){
+//   int arrSize = sizeof(registeredPeers)/sizeof(registeredPeers[0]);
+//   for(int x = 0 ; x < arrSize; x++){
+//     puts(getPrettyMac(registeredPeers[x]));
+//   }
+// }
